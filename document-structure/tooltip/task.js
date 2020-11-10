8642e2597fd6b38d3c539711d2c6f80f4df6181b
laalipop <66365113+laalipop@users.noreply.github.com>
2020-11-10 15:46:49 +0300

    Внесены корректировки

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,17 +1,15 @@
 let tooltipArray = Array.from(document.querySelectorAll('.has-tooltip'));
-let tooltipElement;
+let tooltipElement = document.querySelector('.tooltip');
 
 function showTooltip(event) {
     event.preventDefault();
     let target = event.target;
-    tooltipElement = document.createElement('div');
 
-    tooltipElement.classList.add('tooltip');
     tooltipElement.classList.toggle('tooltip_active');
     tooltipElement.innerHTML = target.title;
     tooltipElement.style.left = this.getBoundingClientRect().left + 'px';
     tooltipElement.style.top = this.getBoundingClientRect().top + 20 + 'px';
-    this.append(tooltipElement);
+    target.after(tooltipElement)
 }
 
 

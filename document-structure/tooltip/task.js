13c03fcfba4ba117e7ee10ca16813123d3b1287d
laalipop <66365113+laalipop@users.noreply.github.com>
2020-11-14 15:51:02 +0300

    Исправлено появление подсказок

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,15 +1,31 @@
 let tooltipArray = Array.from(document.querySelectorAll('.has-tooltip'));
-let tooltipElement = document.querySelector('.tooltip');
+let lastTooltip;
+let currentTooltip;
 
-function showTooltip(event) {
-    event.preventDefault();
+function showTooltip(event) {   
+    let tooltipElement = document.querySelector('.tooltip');
     let target = event.target;
 
-    tooltipElement.classList.toggle('tooltip_active');
-    tooltipElement.innerHTML = target.title;
+    currentTooltip = target.textContent;
+    if (currentTooltip == lastTooltip) {
+        if (tooltipElement.classList.contains('tooltip_active')) {
+            tooltipElement.classList.remove('tooltip_active');
+        } else {
+            tooltipElement.classList.add('tooltip_active');
+        }
+        
+    } else {
+        tooltipElement.classList.remove('tooltip_active');
+        tooltipElement.classList.toggle('tooltip_active');
+    }
+    tooltipElement.innerText = target.title;
     tooltipElement.style.left = this.getBoundingClientRect().left + 'px';
     tooltipElement.style.top = this.getBoundingClientRect().top + 20 + 'px';
-    target.after(tooltipElement)
+    target.after(tooltipElement);
+    
+    lastTooltip = target.textContent;
+
+    event.preventDefault();
 }
 
 
